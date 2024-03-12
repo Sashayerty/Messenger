e2980901d7d4766492f49da5c857d0849144a8ea
Alex <Sashayerty@ya.ru>
2024-03-12 17:03:22 +0300

    Профиль и chatGPT

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 from flask import Flask, redirect, render_template
+import sqlalchemy
 from data.users import User
 from forms.login_form import LoginForm
 from forms.reg_form import RegisterForm
+from forms.chat import ChatForm
 from data import db_session
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from g4f.client import Client
+from g4f.Provider import RetryProvider, Phind, FreeChatgpt, Liaobots
+import g4f.debug
+import json
+
+"""Константы"""
 
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'Sdslg35KO236SafA49F21'
+client = Client(provider=RetryProvider([Phind, Liaobots, FreeChatgpt], shuffle=False)) # создание клиента для chatGPT
+g4f.debug.logging = True
 
-def main():
-    db_session.global_init("db/blogs.db")
-    app.run()
+data = [] # список всех сообщений с chatGPT
+
+def chat_GPT(mess):
+    """Функция для обновления сообщений с chatGPT"""
+    data.append({"role": "user", "content": mess})
+    response = client.chat.completions.create(model="", messages=data)
+    data.append({"role": "assistance", "content": response.choices[0].message.content})
 
 
 @login_manager.user_loader
@@ -28,12 +42,18 @@ def index():
 
 @app.route('/chat')
 def chat():
+  form = ChatForm()
   return render_template('chat.html', title='Чат с User')
 
 
-@app.route('/chatgpt')
+@app.route('/chatgpt', methods=['GET', 'POST'])
+@login_required
 def chatGPT():
-  return render_template('chatgpt.html', title='Чат с ChatGpt')
+  form = ChatForm()
+  if form.validate_on_submit():
+        chat_GPT(form.message.data)
+        return render_template('chatgpt.html', title='Чат с ChatGpt', form=form, messages=data)
+  return render_template('chatgpt.html', title='Чат с ChatGpt', form=form, messages=data)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -59,17 +79,17 @@ def logout():
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def reqister():
+def register():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
             return render_template('register.html', title='Регистрация',
                                    form=form,
                                    message="Пароли не совпадают")
-        if len(form.password.data) < 12:
+        if len(form.password.data) < 8:
            return render_template('register.html', title='Регистрация',
                                    form=form,
-                                   message='len(password) < 12')
+                                   message='len(password) < 8')
         db_sess = db_session.create_session()
         if db_sess.query(User).filter(User.email == form.email.data).first():
             return render_template('register.html', title='Регистрация',
@@ -88,11 +108,19 @@ def reqister():
 
 @app.route('/friends')
 def friends():
-  return render_template('friends.html', title='Друзья')
+    users = ['Sasa', 'Andrew', 'Das']
+    return render_template('friends.html', title='Друзья', users=users)
 
 @app.route('/profile')
+@login_required
 def profile():
-   return render_template('profile.html')
+   db_sess = db_session.create_session()
+   user = db_sess.query(User).filter_by(name=current_user.name).first()
+   return render_template('profile.html', title='Профиль', user=user)
+
+def main():
+    db_session.global_init("db/users.db")
+    app.run()
 
 if __name__ == '__main__':
   main()
